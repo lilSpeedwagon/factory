5ae2e14d694eb4bcf59b1e46b841a7fc116ca0fc
lilSpeedwagon <ded_eg0r@mail.ru>
2020-05-17 14:50:20 +0300

    using CHECK and REQUIRE macros in tests

--- InternalLanguage/InternalLanguage/Runtime_tests/Value_tests.cpp
+++ InternalLanguage/InternalLanguage/Runtime_tests/Value_tests.cpp
@@ -19,28 +19,28 @@ TEST_CASE("Value assign operator", "[Value]")
 	{
 		Value val = 10;
 		REQUIRE(val.getType() == Value::Integer);
-		REQUIRE(val.getValue<int>() == 10);
+		CHECK(val.getValue<int>() == 10);
 	}
 
 	SECTION("Float")
 	{
 		Value val = 10.0f;
 		REQUIRE(val.getType() == Value::Float);
-		REQUIRE(val.getValue<float>() == 10.0f);
+		CHECK(val.getValue<float>() == 10.0f);
 	}
 
 	SECTION("Boolean")
 	{
 		Value val = true;
 		REQUIRE(val.getType() == Value::Boolean);
-		REQUIRE(val.getValue<bool>() == true);
+		CHECK(val.getValue<bool>() == true);
 	}
 
 	SECTION("String")
 	{
 		Value val(std::string("String"));
 		REQUIRE(val.getType() == Value::String);
-		REQUIRE(val.getValue<std::string>() == "String");
+		CHECK(val.getValue<std::string>() == "String");
 	}
 }
 
@@ -52,7 +52,7 @@ TEST_CASE("Value copy constructors", "[Value]")
 		
 		Value val = valInt;
 		REQUIRE(val.getType() == Value::Integer);
-		REQUIRE(val.getValue<int>() == 10);
+		CHECK(val.getValue<int>() == 10);
 	}
 	
 	SECTION("Float")
@@ -61,7 +61,7 @@ TEST_CASE("Value copy constructors", "[Value]")
 
 		Value val = valFloat;
 		REQUIRE(val.getType() == Value::Float);
-		REQUIRE(val.getValue<float>() == 10.0f);
+		CHECK(val.getValue<float>() == 10.0f);
 	}
 
 	SECTION("Boolean")
@@ -70,7 +70,7 @@ TEST_CASE("Value copy constructors", "[Value]")
 
 		Value val = valBool;
 		REQUIRE(val.getType() == Value::Boolean);
-		REQUIRE(val.getValue<bool>() == true);
+		CHECK(val.getValue<bool>() == true);
 	}
 
 	SECTION("String")
@@ -79,21 +79,21 @@ TEST_CASE("Value copy constructors", "[Value]")
 		
 		Value val = valStr;
 		REQUIRE(val.getType() == Value::String);
-		REQUIRE(val.getValue<std::string>() == "String");
+		CHECK(val.getValue<std::string>() == "String");
 	}
 }
 
 TEST_CASE("Value operator!", "[Value]")
 {
-	REQUIRE(Value().operator!().getType() == Value::Boolean);
-	REQUIRE(Value().operator!().getValue<bool>() == true);
-	REQUIRE(Value(true).operator!().getValue<bool>() == false);
-	REQUIRE(Value(0).operator!().getValue<bool>() == true);
-	REQUIRE(Value(1).operator!().getValue<bool>() == false);
-	REQUIRE(Value(0.0f).operator!().getValue<bool>() == true);
-	REQUIRE(Value(-1.0f).operator!().getValue<bool>() == false);
-	REQUIRE(Value(std::string("hallo")).operator!().getValue<bool>() == false);
-	REQUIRE(Value(std::string("")).operator!().getValue<bool>() == true);
+	CHECK(Value().operator!().getType() == Value::Boolean);
+	CHECK(Value().operator!().getValue<bool>() == true);
+	CHECK(Value(true).operator!().getValue<bool>() == false);
+	CHECK(Value(0).operator!().getValue<bool>() == true);
+	CHECK(Value(1).operator!().getValue<bool>() == false);
+	CHECK(Value(0.0f).operator!().getValue<bool>() == true);
+	CHECK(Value(-1.0f).operator!().getValue<bool>() == false);
+	CHECK(Value(std::string("hallo")).operator!().getValue<bool>() == false);
+	CHECK(Value(std::string("")).operator!().getValue<bool>() == true);
 }
 
 TEST_CASE("Value cast", "[Value]")
@@ -104,19 +104,19 @@ TEST_CASE("Value cast", "[Value]")
 		
 		Value intVal = val.toInt();
 		REQUIRE(intVal.getType() == Value::Integer);
-		REQUIRE(intVal.getValue<int>() == 10);
+		CHECK(intVal.getValue<int>() == 10);
 		
 		Value floatVal = val.toFloat();
 		REQUIRE(floatVal.getType() == Value::Float);
-		REQUIRE(floatVal.getValue<float>() == 10.0f);
+		CHECK(floatVal.getValue<float>() == 10.0f);
 		
 		Value boolVal = val.toBool();
 		REQUIRE(boolVal.getType() == Value::Boolean);
-		REQUIRE(boolVal.getValue<bool>() == true);
+		CHECK(boolVal.getValue<bool>() == true);
 		
 		Value stringVal = val.toString();
 		REQUIRE(stringVal.getType() == Value::String);
-		REQUIRE(stringVal.getValue<std::string>() == "10");
+		CHECK(stringVal.getValue<std::string>() == "10");
 	}
 
 	SECTION("Float")
@@ -125,19 +125,19 @@ TEST_CASE("Value cast", "[Value]")
 
 		Value intVal = val.toInt();
 		REQUIRE(intVal.getType() == Value::Integer);
-		REQUIRE(intVal.getValue<int>() == 10);
+		CHECK(intVal.getValue<int>() == 10);
 
 		Value floatVal = val.toFloat();
 		REQUIRE(floatVal.getType() == Value::Float);
-		REQUIRE(floatVal.getValue<float>() == 10.0f);
+		CHECK(floatVal.getValue<float>() == 10.0f);
 
 		Value boolVal = val.toBool();
 		REQUIRE(boolVal.getType() == Value::Boolean);
-		REQUIRE(boolVal.getValue<bool>() == true);
+		CHECK(boolVal.getValue<bool>() == true);
 
 		Value stringVal = val.toString();
 		REQUIRE(stringVal.getType() == Value::String);
-		REQUIRE(stringVal.getValue<std::string>() == "10.000000");
+		CHECK(stringVal.getValue<std::string>() == "10.000000");
 	}
 
 	SECTION("Boolean")
@@ -146,19 +146,19 @@ TEST_CASE("Value cast", "[Value]")
 
 		Value intVal = val.toInt();
 		REQUIRE(intVal.getType() == Value::Integer);
-		REQUIRE(intVal.getValue<int>() == 1);
+		CHECK(intVal.getValue<int>() == 1);
 
 		Value floatVal = val.toFloat();
 		REQUIRE(floatVal.getType() == Value::Float);
-		REQUIRE(floatVal.getValue<float>() == 1.0f);
+		CHECK(floatVal.getValue<float>() == 1.0f);
 
 		Value boolVal = val.toBool();
 		REQUIRE(boolVal.getType() == Value::Boolean);
-		REQUIRE(boolVal.getValue<bool>() == true);
+		CHECK(boolVal.getValue<bool>() == true);
 
 		Value stringVal = val.toString();
 		REQUIRE(stringVal.getType() == Value::String);
-		REQUIRE(stringVal.getValue<std::string>() == "true");
+		CHECK(stringVal.getValue<std::string>() == "true");
 	}
 
 	SECTION("String")
@@ -169,19 +169,19 @@ TEST_CASE("Value cast", "[Value]")
 
 			Value intVal = val.toInt();
 			REQUIRE(intVal.getType() == Value::Integer);
-			REQUIRE(intVal.getValue<int>() == 0);
+			CHECK(intVal.getValue<int>() == 0);
 
 			Value floatVal = val.toFloat();
 			REQUIRE(floatVal.getType() == Value::Float);
-			REQUIRE(floatVal.getValue<float>() == 0.0f);
+			CHECK(floatVal.getValue<float>() == 0.0f);
 
 			Value boolVal = val.toBool();
 			REQUIRE(boolVal.getType() == Value::Boolean);
-			REQUIRE(boolVal.getValue<bool>() == false);
+			CHECK(boolVal.getValue<bool>() == false);
 
 			Value stringVal = val.toString();
 			REQUIRE(stringVal.getType() == Value::String);
-			REQUIRE(stringVal.getValue<std::string>() == "");
+			CHECK(stringVal.getValue<std::string>() == "");
 		}
 		
 		SECTION("string")
@@ -190,19 +190,19 @@ TEST_CASE("Value cast", "[Value]")
 
 			Value intVal = val.toInt();
 			REQUIRE(intVal.getType() == Value::Integer);
-			REQUIRE(intVal.getValue<int>() == 0);
+			CHECK(intVal.getValue<int>() == 0);
 
 			Value floatVal = val.toFloat();
 			REQUIRE(floatVal.getType() == Value::Float);
-			REQUIRE(floatVal.getValue<float>() == 0.0f);
+			CHECK(floatVal.getValue<float>() == 0.0f);
 
 			Value boolVal = val.toBool();
 			REQUIRE(boolVal.getType() == Value::Boolean);
-			REQUIRE(boolVal.getValue<bool>() == true);
+			CHECK(boolVal.getValue<bool>() == true);
 
 			Value stringVal = val.toString();
 			REQUIRE(stringVal.getType() == Value::String);
-			REQUIRE(stringVal.getValue<std::string>() == "string");
+			CHECK(stringVal.getValue<std::string>() == "string");
 		}
 		
 		SECTION("numeric")
@@ -211,19 +211,19 @@ TEST_CASE("Value cast", "[Value]")
 			
 			Value intVal = val.toInt();
 			REQUIRE(intVal.getType() == Value::Integer);
-			REQUIRE(intVal.getValue<int>() == 10);
+			CHECK(intVal.getValue<int>() == 10);
 
 			Value floatVal = val.toFloat();
 			REQUIRE(floatVal.getType() == Value::Float);
-			REQUIRE(floatVal.getValue<float>() == 10.5f);
+			CHECK(floatVal.getValue<float>() == 10.5f);
 
 			Value boolVal = val.toBool();
 			REQUIRE(boolVal.getType() == Value::Boolean);
-			REQUIRE(boolVal.getValue<bool>() == true);
+			CHECK(boolVal.getValue<bool>() == true);
 
 			Value stringVal = val.toString();
 			REQUIRE(stringVal.getType() == Value::String);
-			REQUIRE(stringVal.getValue<std::string>() == "10.5");
+			CHECK(stringVal.getValue<std::string>() == "10.5");
 		}
 		
 	}
@@ -237,7 +237,7 @@ TEST_CASE("Value operator+", "[Value]")
 		Value v2 = 5;
 		Value res = v1 + v2;
 		REQUIRE(res.getType() == Value::Integer);
-		REQUIRE(res.getValue<int>() == 15);
+		CHECK(res.getValue<int>() == 15);
 	}
 
 	SECTION("Float")
@@ -246,7 +246,7 @@ TEST_CASE("Value operator+", "[Value]")
 		Value v2 = 5.5f;
 		Value res = v1 + v2;
 		REQUIRE(res.getType() == Value::Float);
-		REQUIRE(res.getValue<float>() == 15.5f);
+		CHECK(res.getValue<float>() == 15.5f);
 	}
 
 	SECTION("Boolean")
@@ -255,7 +255,7 @@ TEST_CASE("Value operator+", "[Value]")
 		Value v2 = false;
 		Value res = v1 + v2;
 		REQUIRE(res.getType() == Value::Boolean);
-		REQUIRE(res.getValue<bool>() == true);
+		CHECK(res.getValue<bool>() == true);
 	}
 
 	SECTION("String")
@@ -264,7 +264,7 @@ TEST_CASE("Value operator+", "[Value]")
 		Value v2(std::string("world"));
 		Value res = v1 + v2;
 		REQUIRE(res.getType() == Value::String);
-		REQUIRE(res.getValue<std::string>() == "Helloworld");
+		CHECK(res.getValue<std::string>() == "Helloworld");
 	}
 
 	SECTION("Mixed")
@@ -276,27 +276,26 @@ TEST_CASE("Value operator+", "[Value]")
 
 		Value v1 = intVal + floatVal;
 		REQUIRE(v1.getType() == Value::Float);
-		REQUIRE(v1.getValue<float>() == 15.5f);
+		CHECK(v1.getValue<float>() == 15.5f);
 
 		Value v2 = intVal + boolVal;
 		REQUIRE(v2.getType() == Value::Integer);
-		REQUIRE(v2.getValue<int>() == 11);
+		CHECK(v2.getValue<int>() == 11);
 
 		Value v3 = intVal + stringVal;
 		REQUIRE(v3.getType() == Value::String);
-		REQUIRE(v3.getValue<std::string>() == "10str");
+		CHECK(v3.getValue<std::string>() == "10str");
 
 		Value v4 = floatVal + boolVal;
 		REQUIRE(v4.getType() == Value::Float);
-		REQUIRE(v4.getValue<float>() == 6.5f);
+		CHECK(v4.getValue<float>() == 6.5f);
 
 		Value v5 = floatVal + stringVal;
 		REQUIRE(v5.getType() == Value::String);
-		REQUIRE(v5.getValue<std::string>() == "5.500000str");
+		CHECK(v5.getValue<std::string>() == "5.500000str");
 
 		Value v6 = boolVal + stringVal;
 		REQUIRE(v6.getType() == Value::String);
-		REQUIRE(v6.getValue<std::string>() == "truestr");
-	}
-	
+		CHECK(v6.getValue<std::string>() == "truestr");
+	}	
 }
