cf15eafd10a2da657021a6d4fb6badbf2b269433
lilSpeedwagon <ded_eg0r@mail.ru>
2020-09-23 22:21:45 +0300

    camera improvements

--- prototype/FactoryPrototype/Assets/Scripts/cameraScript.cs
+++ prototype/FactoryPrototype/Assets/Scripts/cameraScript.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraScript : MonoBehaviour
@@ -8,13 +6,14 @@ public class cameraScript : MonoBehaviour
     public float MaxCameraSize = 7.5f;
     public float MinCameraSize = 1.0f;
     public float CursorOffsetToMoveScreen = 32;
-    public Vector3 CameraSpeed = new Vector3(0.15f, 0, 0);
+    public Vector3 CameraSpeed = new Vector3(0.4f, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         m_camera = GetComponent<Camera>();
         m_zoomRange = new Range(MinCameraSize, MaxCameraSize);
+        CalculateCurrentSpeed();
     }
 
     // Update is called once per frame
@@ -24,25 +23,25 @@ public class cameraScript : MonoBehaviour
         
         if (mousePos.x <= CursorOffsetToMoveScreen)
         {
-            gameObject.transform.position -= CameraSpeed;
+            gameObject.transform.position -= m_currentSpeed;
         }
         if (mousePos.x >= Screen.width - CursorOffsetToMoveScreen)
         {
-            gameObject.transform.position += CameraSpeed;
+            gameObject.transform.position += m_currentSpeed;
         }
         if (mousePos.y <= CursorOffsetToMoveScreen)
         {
-            gameObject.transform.position -= QuaternionUtils.qRotate90 * CameraSpeed;
+            gameObject.transform.position -= QuaternionUtils.qRotate90 * m_currentSpeed;
         }
         if (mousePos.y >= Screen.height - CursorOffsetToMoveScreen)
         {
-            gameObject.transform.position += QuaternionUtils.qRotate90 * CameraSpeed;
+            gameObject.transform.position += QuaternionUtils.qRotate90 * m_currentSpeed;
         }
 
         if (Input.GetKey(KeyCode.Mouse2))
         {
             var mouseMovement = mousePos - m_prevMousePos;
-            gameObject.transform.position -= mouseMovement * CameraSpeed.magnitude;
+            gameObject.transform.position -= mouseMovement * m_currentSpeed.magnitude;
         }
         m_prevMousePos = mousePos;
     }
@@ -67,9 +66,17 @@ public class cameraScript : MonoBehaviour
         {
             m_camera.orthographicSize = MinCameraSize;
         }
+
+        CalculateCurrentSpeed();
+    }
+
+    void CalculateCurrentSpeed()
+    {
+        m_currentSpeed = CameraSpeed * m_camera.orthographicSize / MinCameraSize;
     }
     
     private Camera m_camera;
+    private Vector3 m_currentSpeed;
     private Range m_zoomRange;
     private Vector3 m_prevMousePos;
 }
