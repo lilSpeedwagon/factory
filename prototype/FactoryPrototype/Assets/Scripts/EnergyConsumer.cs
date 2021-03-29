39928ca1db5989b787778b5391e3cecffc244543
Egor Sorokin <ded_eg0r@mail.ru>
2021-03-29 16:28:16 +0300

    fix energy consumer id

--- prototype/FactoryPrototype/Assets/Scripts/EnergyConsumer.cs
+++ prototype/FactoryPrototype/Assets/Scripts/EnergyConsumer.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class EnergyConsumer : MonoBehaviour
 {
-    public int ConsumerId => g_idCounter;
+    public int ConsumerId { get; }
 
-    public int SourceId { get; set; }
-
+    public int SourceId;
     public int Power;
 
     public EnergyConsumer()
     {
-        m_id = g_idCounter++;
+        ConsumerId = g_idCounter++;
         SourceId = -1;
     }
-
-    private int m_id;
 
     private static int g_idCounter = 0;
 }
